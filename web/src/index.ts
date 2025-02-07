c57468f423fc5f5a05f746b283b56f62e93d82a5
Shaun Ek <shaun@herobullion.com>
2025-02-06 18:10:08 -0700

    Fix logging

--- web/src/index.ts
+++ web/src/index.ts
@@ -36,7 +36,7 @@ async function publish(topicNameOrId: string, data: string) {
   const topic = pubSubClient.topic(topicNameOrId);
 
   try {
-    const messageId = topic.publishMessage({ data: dataBuffer });
+    const messageId = await topic.publishMessage({ data: dataBuffer });
     console.log(`Message ${messageId} published`);
   } catch (error) {
     console.error(`Received error while publishing: ${(error as Error).message}`);
